1d9ce1bca586621d562e9ae33ee95bb5d9eb75fd
Akande Al-ameen <longlasting1805@gmail.com>
2023-12-16 22:33:30 +0100

    first commit

---
+++ src/SumOfXAndY.java
@@ -0,0 +1,10 @@
+public class SumOfXAndY {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 3;
+
+        int sum = x + y;
+
+        System.out.print(++sum);
+    }
+}
